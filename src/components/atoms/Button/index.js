dfc510648fb2eb7266d8e7d9c3e63457dea164ab
Hamzah Arrayhan <hamzaharrayhan@gmail.com>
2021-07-10 11:05:01 +0700

    setup database, upload photo

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {colors} from '../../../utils';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {colors, Fonts} from '../../../utils';
 import IconOnly from './IconOnly';
 import SendButton from './SendButton';
 
@@ -10,6 +10,13 @@ const Button = ({type, judul, onPress, icon, disable}) => {
   }
   if (type === 'icon-only') {
     return <IconOnly icon={icon} onPress={onPress} />;
+  }
+  if (disable) {
+    return (
+      <View style={styles.disableBg}>
+        <Text style={styles.disableTxt}>{judul}</Text>
+      </View>
+    )
   }
   return (
     <TouchableOpacity style={styles.container(type)} onPress={onPress}>
@@ -29,10 +36,21 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingVertical: 10,
   }),
+  disableBg: {
+    backgroundColor: colors.button.disable.background,
+    borderRadius: 10,
+    paddingVertical: 10,
+  },
+  disableTxt: {
+    color: colors.button.disable.text,
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: Fonts.primary[600],    
+  },
   text: type => ({
     textAlign: 'center',
     fontSize: 16,
-    fontWeight: '600',
+    fontFamily: Fonts.primary[600],
     color:
       type === 'Secondary'
         ? colors.button.secondary.text
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import {Button, Gaps, Header, Input} from '../../components';
+import {Button, Gaps, Header, Input, Loading} from '../../components';
 import {colors, useForm} from '../../utils';
 import {Firebase} from '../../config';
+import {showMessage, hideMessage} from 'react-native-flash-message';
 
 const Register = ({navigation}) => {
   const [form, setForm] = useForm({
@@ -12,61 +13,78 @@ const Register = ({navigation}) => {
     password: '',
   });
 
+  const [loading, setLoading] = useState(false);
+
   const onContinue = () => {
     console.log(form);
+    setLoading(true);
     Firebase.auth()
       .createUserWithEmailAndPassword(form.email, form.password)
       .then(userCredential => {
         // Signed in
+        setLoading(false);
+        setForm('reset');
+        const data = {
+          fullName: form.fullName,
+          profession: form.profession,
+          email: form.email,
+        };
+        Firebase.database()
+          .ref('users/' + userCredential.user.uid + '/')
+          .set(data);
         var user = userCredential;
         console.log('success register', user);
-        // ...
       })
       .catch(error => {
+        setLoading(false);
         const errorMessage = error.message;
-        console.log('error register: ', errorMessage);
-        // ..
+        showMessage({
+          message: errorMessage,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
       });
 
     // () => navigation.navigate('UploadPhoto')
   };
   return (
-    <View style={styles.page}>
-      <Header
-        judul="Daftar Akun"
-        onPress={() => navigation.goBack('GetStarted')}
-      />
-      <ScrollView>
-        <View style={styles.content}>
-          <Input
-            label="Full Name"
-            value={form.fullName}
-            onChangeText={value => setForm('fullName', value)}
-          />
-          <Gaps height={24} />
-          <Input
-            label="Pekerjaan"
-            value={form.profession}
-            onChangeText={value => setForm('profession', value)}
-          />
-          <Gaps height={24} />
-          <Input
-            label="Email Address"
-            value={form.email}
-            onChangeText={value => setForm('email', value)}
-          />
-          <Gaps height={24} />
-          <Input
-            label="Password"
-            value={form.password}
-            onChangeText={value => setForm('password', value)}
-            secureTextEntry={true}
-          />
-          <Gaps height={40} />
-          <Button judul="Continue" onPress={onContinue} />
-        </View>
-      </ScrollView>
-    </View>
+    <>
+      <View style={styles.page}>
+        <Header judul="Daftar Akun" onPress={() => navigation.goBack()} />
+        <ScrollView>
+          <View style={styles.content}>
+            <Input
+              label="Full Name"
+              value={form.fullName}
+              onChangeText={value => setForm('fullName', value)}
+            />
+            <Gaps height={24} />
+            <Input
+              label="Pekerjaan"
+              value={form.profession}
+              onChangeText={value => setForm('profession', value)}
+            />
+            <Gaps height={24} />
+            <Input
+              label="Email Address"
+              value={form.email}
+              onChangeText={value => setForm('email', value)}
+            />
+            <Gaps height={24} />
+            <Input
+              label="Password"
+              value={form.password}
+              onChangeText={value => setForm('password', value)}
+              secureTextEntry={true}
+            />
+            <Gaps height={40} />
+            <Button judul="Continue" onPress={onContinue} />
+          </View>
+        </ScrollView>
+      </View>
+      {loading && <Loading />}
+    </>
   );
 };
 
